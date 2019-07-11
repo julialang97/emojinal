81b12b5ee85ce15815ace5c65a927db36a54b8ee
Apple <apple@Apples-MBP-62.attlocal.net>
2019-07-11 16:11:00 -0700

    Iteration 3

--- emojinalProject/ViewController.swift
+++ emojinalProject/ViewController.swift
@@ -16,17 +16,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    var emojis = ["🤰" : "prego", "😔" : "depressed", "🤠" : "yeehaw!"]
+    
+    let customMessages = [
+        "prego" : ["you're prego!! congrats!", "it's time for your weekly checkup!", "It's a GIRL!"],
+        "depressed" : ["I reccomend visiting a therapist hehe", "go get ice cream", "take a stroll and some deep breaths."],
+        "yeehaw!" : ["you're doin great! dont need me!", "Go you!!" ,"YEEEHAWWWW!"]
+    ]
+    
+    
     @IBAction func showMessage(sender: UIButton) {
-        
-        var emojis = ["🤰" : "prego!", "😔" : "sad boi hours", "🤠" : "yeehaw!!"]
         
         let selectedEmoji = sender.titleLabel?.text
         
+        var randomNum = Int.random(in: 0..<3)
         
-        let alertController = UIAlertController(title: "Emojions", message: emojis[selectedEmoji!], preferredStyle: UIAlertController.Style.alert)
+        let emojiMessage = customMessages[emojis[selectedEmoji!]!]?[randomNum]
+        
+        // here I want to extract the word that goes with my emoji
+        // then find that word in the dictionary
+        // get it's value (array)
+        // and access the first element
+        
+        let alertController = UIAlertController(title: "Emojions", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         
-        
+            
+            
         present(alertController, animated: true, completion: nil)
         
         // in as much detail as possible
